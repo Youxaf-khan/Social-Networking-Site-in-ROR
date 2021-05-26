3b39fc1e72fabe6b6e14e48c666a6d325259b66f
Youxaf-khan <Yousafkhan611@gmail.com>
2021-05-26 20:13:55 +0500

    CRUD Implemented (In progress)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to:  'posts#index'
+
   devise_for  :users , path: '/'
-  root to:  'main#index'
+  resources :posts
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
